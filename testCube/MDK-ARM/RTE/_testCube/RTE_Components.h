4fbdbe5bc176a461f3f94c071b9d69142d7cbcd2
Your Name <you@example.com>
2018-10-23 22:33:54 +0700

    Cß║╖c

---
+++ testCube/MDK-ARM/RTE/_testCube/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'testCube' 
+ * Target:  'testCube' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
